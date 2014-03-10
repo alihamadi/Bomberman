0c58f09cb057397045c7a2011eb6a8c5d1b8fb38
Ali Hamadi <ali_hamadi@carleton.ca>
2014-03-09 21:49:02 -0400

    Update MapModel.java

--- src/MapModel.java
+++ src/MapModel.java
@@ -4,15 +4,23 @@
  */
 package bomberman;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import java.util.Scanner; 
 
 /**
  *
@@ -20,15 +28,19 @@ import org.xml.sax.SAXException;
  */
 public class MapModel {
 
+	File mapFile = new File("C:\\Users\\ALI\\workspace\\Bomberman\\src\\map.txt");
+	//private String line;
+	
     MapView viewer;
     Entity mapGrid[][];
     boolean isExitSet = false;
 
     public MapModel(String mapFilePath) {
         if (mapFilePath == null) {
-            generateRandomMap();
+           generateRandomMap();
         } else {
-            loadMapFromFile(mapFilePath);
+        	System.out.println("this is the else");
+            //loadMapFromFile(mapFilePath);
         }
         startMapView();
     }
@@ -66,14 +78,15 @@ public class MapModel {
             for (int x = 0; x < 10; x++) {
                 int rand = (int) (Math.random() * 100);
 
-                if (rand < 15) {             //15% of map is walls
-                    putWallIn(x, y);
+                if (rand >= 50 && rand <= 100) {             //15% of map is walls
+                	putPathIn(x, y);
+                    putExitIn(x, y);  // only one door will be created at a path 
                 } else if (rand >= 15 && rand < 50) {   // 20% of map is boxes
                     putBoxIn(x, y);
-                } else if (rand >= 50 && rand <= 100) {  // 50% of the map will be path
-                    putPathIn(x, y);
-                    putExitIn(x, y);  // only one door will be created at a path 
+                } else if (rand < 15) {  // 50% of the map will be path
+                	putWallIn(x, y); 
                 } else {
+                	System.out.println("wtf");
                     putPathIn(x, y);
                 }
             }
@@ -81,6 +94,57 @@ public class MapModel {
 
 
     }
+    
+    public void makeMap(File file) throws IOException {
+    	System.out.println("the makeMap is being run");
+    	int width, height; 
+        
+    	Scanner reader = new Scanner(new FileReader(file)); 
+          
+        	
+          Scanner line = new Scanner(reader.nextLine());  
+          
+          width = line.nextInt();  
+          height = line.nextInt(); 
+          System.out.println(width);
+          System.out.println(height);
+          
+          mapGrid = new Entity[width][height];
+         // grid = new char[width][height];  
+      
+      
+          for (int y = 0; y < height; y++) {{ 
+        	 while(line.hasNextLine()){
+            line = new Scanner(reader.nextLine());  
+      
+            for (int x = 0; x < width; x++) {
+            	while(line.hasNext()){
+              char c = line.next().charAt(0);  
+      
+              if (c == 'P') {  
+            	  putPathIn(x, y);
+              }
+              else if (c == 'W'){
+                  putWallIn(x, y);  
+              }
+              else if (c == 'B'){
+                  putBoxIn(x, y);  
+              }
+              else if (c == 'E'){
+                  putExitIn(x, y);  
+
+              }
+              else
+            	  System.out.println("Unknown unit in map.txt");
+            } }}}}
+          }  
+      
+          
+        
+       
+    
+
+
 
     public void addPawn(PlayerPawn p) {
     }
